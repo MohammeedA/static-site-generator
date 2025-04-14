bbd85cad86fc94cd4f3308c5a5a4b337ddacef71
Mohammad Al-Sultani <mohammedalsultani@outlook.com>
2025-04-15 01:21:36 +0200

    add test.sh, add test_textnode.py, shorten enum var names

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,10 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = ""
-    BOLD_TEXT = "bold"
-    ITALIC_TEXT = "italic"
-    CODE_TEXT = "code"
+    NORMAL = ""
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
